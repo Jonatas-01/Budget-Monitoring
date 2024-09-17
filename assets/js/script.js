1eda232f5f42cf9f4290a5afd21b36dcce42ff9a
JonatasM <jonatasmc02@gmail.com>
2024-09-18 00:40:16 +0100

    feat: add element to javacript and salary function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,18 @@
 let salaryInput = document.getElementById('salary-input')
 let expensesInput = document.getElementById('expenses-input')
 let categoryInput = document.getElementById('category-input')
+let salaryForm = document.getElementById('salary-form')
+let expensesForm = document.getElementById('expenses-form')
+let salaryBox = document.getElementById('salary-box')
+let expensesBox = document.getElementById('expenses-box')
 
 // Functions
+function salary() {
+    if(salaryInput.value !== ''){
+
+        salaryBox.classList.add('hide')
+        expensesBox.classList.remove('hide')
+    
+    }
+
+}
