aacdad369d4c0ae80da1f44ade85a506c038b7b0
JonatasM <jonatasmc02@gmail.com>
2024-09-18 00:10:16 +0100

    feat: get elements from html to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,6 @@
 // Get Elements
+let salaryInput = document.getElementById('salary-input')
+let expensesInput = document.getElementById('expenses-input')
+let categoryInput = document.getElementById('category-input')
+
+// Functions
