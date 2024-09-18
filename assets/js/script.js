c5e9e0479ab69353bd6070e5d646afe59e30636d
JonatasM <jonatasmc02@gmail.com>
2024-09-18 11:35:37 +0100

    feat: create elements in function salary

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,23 @@ let salaryForm = document.getElementById('salary-form')
 let expensesForm = document.getElementById('expenses-form')
 let salaryBox = document.getElementById('salary-box')
 let expensesBox = document.getElementById('expenses-box')
+let tableDiv = document.getElementById('table-div')
+let tableHeader = document.getElementById('table-header')
 
 // Functions
 function salary() {
     if(salaryInput.value !== ''){
         salaryBox.classList.add('hide')
         expensesBox.classList.remove('hide')
+        tableDiv.classList.remove('hide')
     
+        let tableFP = document.createElement('p')
+        tableFP.innerHTML = `Salary: ${salaryInput.value}`
+        tableHeader.appendChild(tableFP)
+
+        let tableSP = document.createElement('p')
+        tableSP.innerHTML = `Balance: ${salaryInput.value} `
+        tableHeader.appendChild(tableSP)
     }
 
 }
